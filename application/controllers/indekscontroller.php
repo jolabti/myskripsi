e1eb8ba5be2e93d5d096bf98ac631c97936dbc0b
jhngolan <joparsip@gmail.com>
2016-05-21 13:48:25 +0700

    21 mei 2016

--- application/controllers/indekscontroller.php
+++ application/controllers/indekscontroller.php
@@ -19,9 +19,63 @@ class Indekscontroller extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$this->load->view('login_v');
 	}
-	
+
+	public function loginc()
+	{
+		//$this->load->view('login_v');
+		$this->load->library('form_validation');
+
+					$this->form_validation->set_rules('username', 'Username', 'trim|required|xss_clean');
+					$this->form_validation->set_rules('password', 'Password', 'trim|required|xss_clean|callback_check_database');
+
+					if($this->form_validation->run() == FALSE)
+					{
+						//Field validation failed.  User redirected to login page
+						$this->load->view('login_view');
+					}
+					else
+					{
+						//Go to private area
+						redirect('home', 'refresh');
+					}
+
+	}
+
+	function check_database($password)
+  {
+   //Field validation succeeded.  Validate against database
+	 $this->load->model('user');	
+   $username = $this->input->post('username');
+
+   //query the database
+   $result = $this->user->login($username, $password);
+
+   if($result)
+   {
+       $sess_array = array();
+       foreach($result as $row)
+     {
+        $sess_array = array(
+           'id' => $row->id,
+           'username' => $row->username
+         );
+       $this->session->set_userdata('logged_in', $sess_array);
+       }
+       return TRUE;
+
+		}
+
+		 else
+     {
+     $this->form_validation->set_message('check_database', 'Invalid username or password');
+     return false;
+     }
+ }
+
+
+
 }
 
 /* End of file welcome.php */
---
+++ application/views/login_v.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title>LOGIN</title>
+  <link rel="stylesheet" href="<?php echo base_url();?>css/bdmcss/bootstrap.css" media="screen">
+  <link rel="stylesheet" href="<?php echo base_url();?>css/bdmcss/mdb.css" media="screen">
+</head>
+
+<body>
+
+
+  <!--Navbar-->
+  <nav class="navbar navbar-light info-color navbar-toggleable-md ">
+
+    <!-- Collapse button-->
+    <button class="navbar-toggler hidden-sm-up" type="button" data-toggle="collapse" data-target="#collapseEx2">
+      <i class="fa fa-bars"></i>
+    </button>
+
+    <div class="container  ">
+
+      <!--Collapse content-->
+
+      <a class="navbar-brand text-lighten-5 text-fluid" href="#">SISTEM PENUNJANG KEPUTUSAN - CALAS LABORATORIUM TEKNIK INFORMATIKA</a>
+      <!--Links-->
+
+      <!--Search form-->
+
+
+      <!--/.Collapse content-->
+
+    </div>
+
+  </nav>
+
+  <div class="container card-danger ">
+
+    <!--Card-->
+    <div class="card">
+
+      <!--Card image-->
+      <img class="img-rounded" src="<?php echo base_url();?>media/ugfuture.jpg" alt="Card image cap" width="100%" height="350px">
+      <!--/.Card image-->
+
+      <!--Card content-->
+      <div class="card-block">
+        <!--Title-->
+        <h4 class="card-title">SISTEM LOGIN</h4>
+        <!--Text-->
+
+        <div class="md-form">
+          <form class="form-control-sm " action="index.html" method="post">
+            <input placeholder="Username" type="text" id="form5" class="form-control">
+            <input placeholder="Password" type="password" id="form5" class="form-control">
+          <input type="submit" name="name" value="LOGIN" class="form-control btn btn-sm btn-info-outline">
+          </form>
+
+
+
+        </div>
+
+
+
+        <div class="card">
+
+          <div class="card-block">
+            <h4 class="card-title">Keterangan</h4>
+            <p class="card-text">Jika terdapat kesalahan program- Klik button di bawah.</p>
+            <a href="#" class="btn btn-danger">Report Bug</a>
+          </div>
+        </div>
+      </div>
+      <!--/.Card content-->
+
+    </div>
+    <!--/.Card-->
+
+  </div>
+
+
+
+
+</body>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script type="text/javascript" src="<?php echo base_url();?>css/bdmjs/jquery-2.2.3.js">
+</script>
+<script type="text/javascript" src="<?php echo base_url();?>css/bdmjs/js/mdb.js">
+</script>
+<script type="text/javascript" src="<?php echo base_url();?>css/bdmjs/js/bootstrap.js">
+</script>
+<script type="text/javascript" src="<?php echo base_url();?>css/bdmjs/js/tether.js">
+</script>
+
+</html>
