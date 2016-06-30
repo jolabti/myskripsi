bb9e9bdf07d07654ebc84e78688bc205106c4c41
jhngolan <joparsip@gmail.com>
2016-07-01 06:24:35 +0700

    1 Juli 2016

--- application/controllers/HimpunanController.php
+++ application/controllers/HimpunanController.php
@@ -16,7 +16,14 @@ class HimpunanController extends CI_Controller{
 
   public function himpunanJson(){
           $this->load->model('Himpunan');
-       
+
+          header('Access-Control-Allow-Origin: *');
+          header('Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method');
+          header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE');
+          $method = $_SERVER['REQUEST_METHOD'];
+          if ($method == 'OPTIONS') {
+              die();
+          }
           $data= $this->Himpunan->ambilHimpunanJson();
 
           print json_encode($data);
